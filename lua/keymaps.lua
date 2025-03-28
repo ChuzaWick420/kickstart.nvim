abbebd385831043c334d0892ab1ff7743018b53f
ChuzaWick420 <zaiddadev@gmail.com>
2025-03-29 01:00:18 +0500

    Added: keymaps for page navigation

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -56,4 +56,8 @@ vim.keymap.set('n', '<leader>e', vim.cmd.NvimTreeToggle, { desc = 'Toggle nvim t
 
 vim.keymap.set('n', '<leader>m', vim.cmd.MarkdownPreview, { desc = 'Open Markdown Preview' })
 
+vim.keymap.set('n', '<M-j>', '<C-d>zz', { desc = 'Jump page down' })
+
+vim.keymap.set('n', '<M-k>', '<C-u>zz', { desc = 'Jump page down' })
+
 -- vim: ts=2 sts=2 sw=2 et
